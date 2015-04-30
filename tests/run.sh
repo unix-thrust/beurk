b56abc019757edfcef41f36d983738409fb2fd92
mzap <mzapata@student.42.fr>
2015-04-30 20:55:50 +0200

    test(core): add commit history checker
    add to general test suite

--- tests/run.sh
+++ tests/run.sh
@@ -2,5 +2,14 @@
 
 ROOTDIR=$(git rev-parse --show-toplevel)
 TESTDIR=${ROOTDIR}/tests
+array=( "ci" "core-unittest" )
+err_count=0
 
-${TESTDIR}/core-unittest/run.sh
+for element in ${array[@]}
+do
+  ${TESTDIR}/$element/run.sh
+  (( err_count += $? ))
+done
+if [ $err_count -gt 0 ]; then
+  exit 1
+fi
