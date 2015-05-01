5eb1c4466d97a939225d9cb5cb91eb78d98c590a
nil0x42 <nil0x42@gmail.com>
2015-05-01 14:29:43 +0200

    chore(shell-scripts): change shebang to /bin/bash
    using bash as shell script language ensures that script
    could be correctly executed on platforms linkking sh to `csh`
    or `zsh`.

--- tests/ci/check_commits_history.sh
+++ tests/ci/check_commits_history.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ROOTDIR=$(git rev-parse --show-toplevel)
 count=0
@@ -8,8 +8,8 @@ while $(git log HEAD~$count -n 1 --pretty="%B" &> /dev/null);
 do
   git log HEAD~$count -n 1 --pretty="%B" \
       | python2 $ROOTDIR/utils/commit-msg.py
-  err_count=$(($err_count+$?))
-  count=$(($count+1))
+  err_count=`expr "$err_count" + "$?"`
+  count=`expr "$count" + "1"`
 done
 if [ $err_count -gt 0 ]; then
   exit 1
