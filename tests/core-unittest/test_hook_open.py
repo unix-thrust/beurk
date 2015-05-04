cf48b75240c7a55226e50acd080a7b9a5a42cc94
nil0x42 <nil0x42@gmail.com>
2015-05-05 01:03:43 +0200

    test(core): improve test_hook_open.py

--- tests/core-unittest/test_hook_open.py
+++ tests/core-unittest/test_hook_open.py
@@ -13,29 +13,23 @@ def unittest(*args):
     global tests
     tests += 1
     cmd =  "gcc src/%s -o %s" % (args[0]+".c", args[0])
-    print cmd
     ret = commands.getstatusoutput(cmd)
-    print ret
     if ret[0] == 0:
         cmd = "LD_PRELOAD=" + ROOTDIR + "/libselinux.so "
         cmd += "./" + " ".join([str(x) for x in args])
         cmd += " 2>&1"
-        print cmd
         ret = commands.getstatusoutput(cmd)
         os.unlink(args[0])
-    if ret[0]:
+    if ret[0] or "[BEURK_ERROR" in ret[1]:
+        print("BLA")
         errors += 1
-    return False if "[BEURK_ERROR" in ret[1] else True
+        return False
+    return True
 
 unittest("open", "test1", os.O_CREAT, 0o777)
 os.unlink("test1")
 
-
-
-
-
-
-# END
+# FOOTER
 if errors:
     sys.exit("[-] Some tests failed (%d of %d)" % (errors, tests))
 else:
