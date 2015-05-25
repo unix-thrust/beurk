d43cd3be2f5872803ee3715007fb51c94290a3fa
mzap <mzapata@student.42.fr>
2015-05-25 18:41:54 +0200

    test(core): basic hook testing

---
+++ tests/core/hooks/readdir.c
@@ -0,0 +1,7 @@
+#include <dirent.h>
+
+int     main(void) {
+    DIR tmp;
+
+    readdir(&tmp);
+}
---
+++ tests/core/hooks/stat.c
@@ -0,0 +1,8 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(void) {
+    struct stat buf;
+    stat("tata", &buf);
+}
