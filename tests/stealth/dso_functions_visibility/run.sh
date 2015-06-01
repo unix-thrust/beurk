19d755b21055c59b3680b1b56fcc0e7b1ff8c0ee
mzap <mzapata@student.42.fr>
2015-06-02 00:58:50 +0200

    test(core): fix tests that fails on some platform

--- tests/stealth/dso_functions_visibility/run.sh
+++ tests/stealth/dso_functions_visibility/run.sh
@@ -2,9 +2,8 @@
 
 # test: dso_functions_visibility
 #
-# Verify that internal API functions are NOT
-# callable when DSO (libselinux.so) is compiled
-# normally (in non debug mode)
+# Verify that internal API functions are NOT callable when
+# DSO (libselinux.so) is compiled normally (in non debug mode)
 
 set -ve
 cd `dirname $0`
@@ -14,23 +13,23 @@ trap "rm -f ./test" EXIT
 
 
 # ensure that without debug mode, compilation fails with
-# something like `undefined reference to is_hidden_file`
+# something like `undefined reference to is_hidden_file()`
 BEURK_DEBUG_LEVEL=0 make re -C "${ROOTDIR}"
 cc -I${ROOTDIR}/includes -L${ROOTDIR} -lselinux test.c -o test 2>&1 \
     | grep -i 'undefined reference' | grep "is_hidden_file"
 
 
-# ensure that with debug level 1, compilation don't fails, and
-# the `called fn()` beurk debug lines are here
+# ensure that with debug level 1, compilation doesn't fail, and
+# `called fn()` beurk debug lines are here
 BEURK_DEBUG_LEVEL=1 make re -C "${ROOTDIR}"
-cc -I${ROOTDIR}/includes -L${ROOTDIR} -lselinux test.c -o test
+cc -I${ROOTDIR}/includes test.c -L${ROOTDIR} -lselinux -o test
 LD_PRELOAD=${ROOTDIR}/libselinux.so ./test 2>&1 \
     | grep -i "called is_hidden_file()"
 
 
-# ensure that with debug level 2, compilation don't fails, and
-# the `called fn()` beurk debug lines are here
+# ensure that with debug level 2, compilation doesn't fail, and
+# `called fn()` beurk debug lines are here
 BEURK_DEBUG_LEVEL=2 make re -C "${ROOTDIR}"
-cc -I${ROOTDIR}/includes -L${ROOTDIR} -lselinux test.c -o test
+cc -I${ROOTDIR}/includes test.c -L${ROOTDIR} -lselinux -o test
 LD_PRELOAD=${ROOTDIR}/libselinux.so ./test 2>&1 \
     | grep -i "called is_hidden_file()"
