99a7865306cce98f05fe5729f9931d9e45f2622e
nil0x42 <nil0x42@gmail.com>
2015-06-07 18:49:24 +0200

    test(core): refactor internal-api tests
    the tests previously made by tests/core/check_needed_unit_tests.sh
    are now done directly in tests/core/internal-api/run.sh
    
    the test now also fully checks that a test is available and
    called for each internal api function.

--- tests/core/internal-api/run.sh
+++ tests/core/internal-api/run.sh
@@ -3,20 +3,46 @@
 set -o errexit
 set -o verbose
 
-if [ `uname -s` = "FreeBSD" ]; then
-    MAKE=gmake
-else
-    MAKE=make
-fi
+function print_good () {
+    echo -e "\033[1;32m[+]\033[0;32m $1\033[0m"
+}
+function print_bad () {
+    echo -e "\033[1;31m[-]\033[0;31m $1\033[0m"
+}
 
+# use gmake on freebsd
+[ `uname -s` = "FreeBSD" ] && MAKE=gmake || MAKE=make
+
+# set ROOTDIR and current dir
 ROOTDIR=$(git rev-parse --show-toplevel)
 cd `dirname $0`
 
+# ensure that a .c file exists for each internal api source file
+# e.g: src/is_attacker.c MUST have a corresponding .c file in current dir
+EXCLUDE_FILES=" init.c debug.c config.c "
+for file in $(find $ROOTDIR/src/ -maxdepth 1 -type f -name '*.c' -exec basename {} \;); do
+    [[ "$EXCLUDE_FILES" =~ ' '$file' ' ]] && continue
+    func="test_${file%.*}"
+    # ensure internal-api source file has a test dedicated file
+    if [ ! -f "$file" ]; then
+        print_bad "missing test file: '$file' !"
+        (( ++errors ))
+    # ensure test function is called in main.c
+    elif ! grep -Eq '^[[:space:]]*'"$func"'\(' main.c; then
+        print_bad "'$func' is not called in main.c !"
+        (( ++errors ))
+    else
+        print_good "'$file' OK"
+    fi
+done
+
+[ $errors -ne 0 ] && exit 1
+
 # distclean at exit
-trap "make distclean || exit 1" EXIT
+trap "${MAKE} distclean" EXIT
 
 # rebuild test
-make re
+${MAKE} re
 
 # use BEURK DSO library path
 export LD_LIBRARY_PATH=${ROOTDIR}:${LD_LIBRARY_PATH}
