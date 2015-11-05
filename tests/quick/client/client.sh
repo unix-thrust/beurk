8c11dff1661ded620e09bd97ca725907431905ae
nil0x42 <nil0x42@gmail.com>
2015-11-06 00:03:35 +0100

    test(client): update client output test
    As we where unable to detect Bind errors from client when launched
    with python version 2.6, the "Bind error" output test is disabled
    in this specific case.

--- tests/quick/client/client.sh
+++ tests/quick/client/client.sh
@@ -3,12 +3,27 @@
 set -ve
 cd `git rev-parse --show-toplevel`
 
+# Ensure that client launched without arguments returns nonzero
 ! ./client.py
+
+# Try to pass an invalid file as batch file
 ./client.py host port bind_port pass missing_file.txt | grep "cannot open"
+
+# Ensure running client without arguments prints the correct help message
 ./client.py | grep "client.py <address> <port> <bind_port> \[password\] \[batch_file | -c command\]"
+
+# Check 'Socket error' output error
 ./client.py beurk 3005 6666 2>&1 |
     grep "host \[beurk\], port \[3005\], bind port \[6666\] : Socket error"
-./client.py 127.7.7.7 3005 77777 2>&1 |
-    grep "host \[127.7.7.7\], port \[3005\], bind port \[77777\] : Bind error"
+
+# Check 'Connexion refused' output error
 ./client.py 127.0.0.1 3005 64835 2>&1 |
     grep "host \[127.0.0.1\], port \[3005\], bind port \[64835\] : Connection refused"
+
+# As we were unable to distinguish "Bind Error" from "Connection refused"
+# errors with python 2.6, this test is disabled for this version of python
+if python --version 2>&1 | grep -Eqv '^Python 2\.6'; then
+    # check 'Bind Error' output error
+    ./client.py 127.7.7.7 3005 77777 2>&1 |
+        grep "host \[127.7.7.7\], port \[3005\], bind port \[77777\] : Bind error"
+fi
